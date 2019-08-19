b90581b87d12e87e984d4a84df38aacb8029a3e2
zh-plus <11610127@mail.sustech.edu.cn>
2019-08-19 20:52:34 +0800

    Leetcode sort color medium question done.

--- LeetCode/medium/SortColors.cpp
+++ LeetCode/medium/SortColors.cpp
@@ -3,9 +3,9 @@
 //
 #include <vector>
 #include <iostream>
-//#include <fstream>
-#include <sstream>
 #include <fstream>
+#include <sstream>
+#include <algorithm>
 
 #define BOOST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 #define LOCAL
@@ -14,9 +14,31 @@ using namespace std;
 
 class Solution {
 public:
-    void sortColors(vector<int> &nums) {
+    void sortColors1(vector<int> &nums) {
+        sort(nums.begin(), nums.end(), [](auto x, auto y) { return y - x > 0; });
+    }
 
+    void sortColors(vector<int> &nums){
+        vector<int> sorted_nums(nums.size());
+        int p = 0, q = nums.size() - 1;
+        for (auto x: nums) {
+            switch (x) {
+                case 0:
+                    sorted_nums[p++] = x;
+                    break;
+                case 2:
+                    sorted_nums[q--] = x;
+                    break;
+                default:
+                    break;
+            }
+        }
 
+        for (int i = p; i <= q; ++i) {
+            sorted_nums[i] = 1;
+        }
+
+        nums = sorted_nums;
     }
 };
 
@@ -31,12 +53,16 @@ int main() {
     vector<int> nums;
 
     string line;
+
+    getline(cin, line);
     istringstream ss(line);
     int num;
     while (ss >> num) {
         nums.push_back(num);
     }
 
+    Solution s;
+    s.sortColors(nums);
     for (auto x: nums) {
         cout << x << " ";
     }
