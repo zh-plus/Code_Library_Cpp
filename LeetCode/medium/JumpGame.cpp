71526764d53b35aac43dc2643fece9e9e5c260d2
zh-plus <11610127@mail.sustech.edu.cn>
2019-08-24 21:32:30 +0800

    Done a LeetCode medium Jump Game problem in foolish way.

---
+++ LeetCode/medium/JumpGame.cpp
@@ -0,0 +1,77 @@
+//
+// Created by 10578 on 8/24/2019.
+//
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+
+#define BOOST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define LOCAL
+
+using namespace std;
+
+class Solution {
+public:
+    bool *cant;
+
+    bool canJump(vector<int> &nums){
+
+    }
+
+    bool canJump1(vector<int> &nums) {
+        cant = new bool[nums.size()]();
+
+        bool result = dfs(nums, 0);
+
+        return result;
+    }
+
+    bool dfs(vector<int> &nums, int start) {
+        if (start >= nums.size()) {
+            return false;
+        }
+
+        if (start == nums.size() - 1) {
+            return true;
+        }
+
+        for (int i = min((int)nums.size() - 1 - start, nums[start]); i > 0; --i) {
+            if (cant[start + i])
+                continue;
+
+            if (dfs(nums, start + i)) {
+                return true;
+            } else {
+                cant[start + i] = true;
+            }
+        }
+
+        return false;
+    }
+};
+
+
+int main() {
+    BOOST_IO;
+
+#ifdef LOCAL
+    fstream in("in.txt");
+    cin.rdbuf(in.rdbuf());
+#endif
+
+    string line;
+    getline(cin, line);
+    istringstream input(line);
+
+    vector<int> nums;
+    int num;
+    while (input >> num) {
+        nums.push_back(num);
+    }
+
+    Solution s;
+    auto result = s.canJump(nums);
+
+    cout << (result ? "true" : "false");
+}
