bdb806fe32ad8aa3efbb42079c100d9b79aaefaf
zh-plus <11610127@mail.sustech.edu.cn>
2019-11-19 01:58:44 +0800

    Done 2 sundry c++ study in resource management

---
+++ study/sundries/copy_and_swap_idiom.cpp
@@ -0,0 +1,87 @@
+//
+// Created by 10578 on 11/19/2019.
+//
+
+/**
+ * https://stackoverflow.com/a/3279550
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm> // std::copy
+#include <cstddef> // std::size_t
+
+using namespace std;
+
+class DumbArray {
+public:
+	// (default) constructor
+	explicit DumbArray(std::size_t size = 0)
+			: mSize(size),
+			  mArray(mSize ? new int[mSize]() : nullptr) {
+	}
+
+	// copy-constructor
+	DumbArray(const DumbArray &other)
+			: mSize(other.mSize),
+			  mArray(mSize ? new int[mSize] : nullptr) {
+		cout << "Copy constructor" << endl;
+		// note that this is non-throwing, because of the data
+		// types being used; more attention to detail with regards
+		// to exceptions must be given in a more general case, however
+		std::copy(other.mArray, other.mArray + mSize, mArray);
+	}
+
+	// copy-assignment
+	DumbArray &operator=(DumbArray other) {
+		cout << "Copy assignment" << endl;
+		swap(*this, other);
+
+		return *this;
+	}
+
+	friend void swap(DumbArray &first, DumbArray &second) {
+		cout << "Swap" << endl;
+		using std::swap;
+
+		swap(first.mSize, second.mSize);
+		swap(first.mArray, second.mArray);
+	}
+
+	// destructor
+	~DumbArray() {
+		delete[] mArray;
+	}
+
+private:
+	std::size_t mSize;
+	int *mArray;
+};
+
+
+template<typename Sequence>
+void print_vector(Sequence s) {
+	using namespace std;
+
+	for (auto &x: s) {
+		cout << x << " ";
+	}
+	cout << endl;
+}
+
+int main() {
+	using namespace std;
+
+	vector v1{1, 2, 3, 4};
+	vector v2{5, 6, 7};
+	swap(v1, v2);
+
+	print_vector(v1);
+	print_vector(v2);
+
+	DumbArray a1(2);
+	DumbArray a3(3);
+	a3 = a1;
+
+	return 0;
+}
---
+++ study/sundries/rule_of_three.cpp
@@ -0,0 +1,66 @@
+//
+// Created by 10578 on 11/18/2019.
+//
+
+/**
+ * https://stackoverflow.com/a/4172724
+ */
+
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <string>
+
+using namespace std;
+
+class Person {
+public:
+	char *name;
+	int age;
+
+	// the constructor acquires a resource:
+	// in this case, dynamic memory obtained via new[]
+	Person(const char *the_name, int the_age) {
+		name = new char[strlen(the_name) + 1];
+		strcpy(name, the_name);
+		age = the_age;
+	}
+
+	Person(const Person &other) {
+		cout << "Copy constructor" << endl;
+		name = new char[strlen(other.name) + 1];
+		strcpy(name, other.name);
+		age = other.age;
+	}
+
+	Person &operator=(const Person &other) {
+		cout << "Copy assignment" << endl;
+		char *new_name = new char[strlen(other.name) + 1];
+		strcpy(new_name, other.name);
+
+		delete[](name);
+		name = new_name;
+		age = other.age;
+
+		return *this;
+	}
+
+
+	// the destructor must release this resource via delete[]
+	~Person() {
+		delete[] name;
+	}
+};
+
+int main() {
+	Person a("Bjarne Stroustrup", 60);
+	Person b(a);   // What happens here?
+	Person c(b);         // And here?
+	c = c;
+
+	strcpy(a.name, "zhenghao");
+	cout << "a:" << a.name << " -- " << "b:" << b.name << " -- " << "c:" << c.name << endl;
+
+
+	return 0;
+}
