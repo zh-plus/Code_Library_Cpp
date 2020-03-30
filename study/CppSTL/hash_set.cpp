2d7a43c521818ae9bbea302109ab850f0dec05b4
Hao Zheng <zhenghaosustc@gmail.com>
2020-03-30 22:34:41 +0800

    Add unordered_set STL study

--- study/CppSTL/hash_set.cpp
+++ study/CppSTL/hash_set.cpp
@@ -6,9 +6,8 @@
 #include <queue>
 #include <vector>
 #include <iostream>
-#include <iterator>
 #include <fstream>
-#include <set>
+#include <unordered_set>
 
 #define BOOST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 #define ALTER_IN(filename) fstream in_f(filename); cin.rdbuf(in_f.rdbuf());
@@ -22,6 +21,12 @@ void print_set(T &s) {
         cout << x << " ";
     }
     cout << endl;
+}
+
+template<class T>
+inline void hash_combine(std::size_t &seed, const T &v) {
+    std::hash<T> hasher;
+    seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
 }
 
 class A {
@@ -40,29 +45,26 @@ public:
     }
 };
 
+namespace std {
+    template<>
+    struct hash<A> {
+        size_t operator()(const A &a) const {
+            size_t seed = 0;
+            hash_combine(seed, a.u);
+            hash_combine(seed, a.v);
+            return seed;
+        }
+    };
+}
 
 int main() {
     BOOST_IO;
 
-    auto cmp = [] (const A &a1, const A &a2) -> bool {
-        return tie(a1.u, a1.v) < tie(a2.u, a2.v);
-    };
-    set<A, decltype(cmp)> set1;
-    for (auto p: {A{1, 2}, A{1, 4}, A{2, 3}, A{2, 3}, A{1, 2}}) {
-        cout << "Inserting " << p << endl;
-        set1.insert(p);
+    unordered_set<A> set1;
+    for (auto p: {pair{1, 2}, pair{1, 4}, pair{2, 3}, pair{2, 3}, pair{1, 2}}) {
+        set1.emplace(p.first, p.second);
     }
     print_set(set1);
-
-//    auto comp = [](int x, int y) { return x < y; };
-//    auto set = std::set<int, decltype(comp)>(comp);
-//
-//    set.insert(1);
-//    set.insert(10);
-//    set.insert(1); // Dupe!
-//    set.insert(2);
-//
-//    std::copy(set.begin(), set.end(), ostream_iterator<int>(std::cout, "\n"));
 
     return 0;
 }
