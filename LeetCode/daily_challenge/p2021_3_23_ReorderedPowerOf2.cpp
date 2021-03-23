7d0381b0670c8c860cdb6d7d8b6137d31e94d95e
Hao Zheng <zhenghaosustc@gmail.com>
2021-03-23 18:40:29 +0800

    Done a Common problem in LeetCode Daily Challenge.

--- LeetCode/daily_challenge/p2021_3_23_ReorderedPowerOf2.cpp
+++ LeetCode/daily_challenge/p2021_3_23_ReorderedPowerOf2.cpp
@@ -19,13 +19,37 @@ using namespace std;
 class Solution {
 public:
     bool reorderedPowerOf2(int N) {
+        auto num_repr = get_num_repr(N);
 
+        for (auto i = 0; i < 30; ++i) {
+            if (get_num_repr(pow(2, i)) == num_repr) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
-    vector<int> get_
+    vector<int> get_num_repr(int N) {
+        if (N == 0) {
+            return vector<int>{1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        }
+
+        // 0123456789 frequency
+        vector<int> v(10, 0);
+        while (N != 0) {
+            ++v[N % 10];
+            N /= 10;
+        }
+
+        return v;
+    }
 };
 
-int main(){
+int main() {
+
+    Solution s;
+    cout << s.reorderedPowerOf2(10);
 
     return 0;
 }
