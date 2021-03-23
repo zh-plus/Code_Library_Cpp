2d0546370e68a66b552efdda9115971a7d3f42c4
Hao Zheng <zhenghaosustc@gmail.com>
2021-03-23 17:27:14 +0800

    Update

---
+++ LeetCode/daily_challenge/p2021_3_23_ReorderedPowerOf2.cpp
@@ -0,0 +1,31 @@
+//
+// Created by 10578 on 2021/3/23.
+//
+
+#include "iostream"
+#include "vector"
+#include "iterator"
+#include "string"
+#include "algorithm"
+#include "numeric"
+#include "limits"
+#include "sstream"
+#include "fstream"
+
+#define ALTER_IN(filename) std::fstream fs(filename); std::cin.rdbuf(fs.rdbuf());
+
+using namespace std;
+
+class Solution {
+public:
+    bool reorderedPowerOf2(int N) {
+
+    }
+
+    vector<int> get_
+};
+
+int main(){
+
+    return 0;
+}
